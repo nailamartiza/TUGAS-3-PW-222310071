2f2feffde581ed4e0dc2d0cd85ec6974f0d7a8af
nailamartiza <113446150+nailamartiza@users.noreply.github.com>
2024-04-12 18:39:59 +0700

    Add files via upload

---
+++ storage/framework/views/4b19fbd18b9aabe93ebfdf9b8916b357.php
@@ -0,0 +1,5 @@
+<?php $__env->startSection('title', __('Not Found')); ?>
+<?php $__env->startSection('code', '404'); ?>
+<?php $__env->startSection('message', __('Not Found')); ?>
+
+<?php echo $__env->make('errors::minimal', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\NAILA\COLLEGE\SEMESTER 4\MATERI\PEMROGRAMAN WEB\LAB\Project_PW\myprojects\example-app\vendor\laravel\framework\src\Illuminate\Foundation\Exceptions/views/404.blade.php ENDPATH**/ ?>
